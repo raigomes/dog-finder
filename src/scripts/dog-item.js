952de83c7cd3ec0def61790007b45567fca089d7
raigomes <rai.gomes.ismart@gmail.com>
2018-11-13 00:24:08 -0200

    Updates

--- src/scripts/dog-item.js
+++
@@ -1,6 +0,0 @@
-export default module `<div class="dog-list-item" data-dog-id="1">
-        <img src="images/6.jpg" alt="Foto do Catioro" class="dog-photo">
-        <div class="dog-name">Vira Lata</div>
-        <div class="dog-temperament">Escandaloso, carente, disperso, medroso</div>
-    </div>
-`
