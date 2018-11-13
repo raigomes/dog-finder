6f9b5fe53392036e8cebdfbc8c4b62be0ca3debc
raigomes <rai.gomes.ismart@gmail.com>
2018-11-13 21:21:21 -0200

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,35 @@
+function openJSON (json, callback) {
+    let request = new XMLHttpRequest()
+    request.open('GET', json, true)
+    
+    request.onload = () => {
+        if (request.status >= 200 && request.status < 400) {
+            var data = JSON.parse(request.responseText)
+            if (calback) callback(data);
+        } else {                
+            console.log('Falha na requisição http: ', request.status, request.statusText)
+        }            
+    };
+
+    request.onerror = () => {
+        console.log("ERRO ao retornar conteúdo JSON")
+    };
+    
+    request.send()
+}
+
 function fillDogList() {
     let containerNode = document.querySelector(".dog-container")
     let list = document.querySelector(".dog-list")
     
     if (list === null || list === undefined ) {
         list = document.createElement("div")
-        list.setAttribute("class", "dog-list");
-    }
+        list.setAttribute("class", "dog-list")
+    }    
 
-    let request = new XMLHttpRequest();
-    request.open('GET', 'json/breeds.json', true);
-    
-    request.onload = () => {
-        if (request.status >= 200 && request.status < 400) {
-            //containerNode.innerHTML = list;
-            console.log('success!', request);
-        } else {                
-            console.log('The request failed!');
-        }            
-    };
-
-    request.onerror = () => {
-        console.log("ERRO ao retornar conteúdo JSON");
-    };
-    
-    request.send();    
+    openJSON('json/breeds.json', (data) => {
+        console.log(data)
+    })
 }
 
 document.onreadystatechange = function () {
