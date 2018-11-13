3e796e3edf2b0f0ec6dbb48e087151724cd8e964
raigomes <rai.gomes.ismart@gmail.com>
2018-11-13 21:58:22 -0200

    Updates

--- scripts/dog-item.js
+++ scripts/dog-item.js
@@ -1,6 +1,7 @@
-export default module `<div class="dog-list-item" data-dog-id="1">
+var item = `<div class="dog-list-item" data-dog-id="1">
         <img src="images/6.jpg" alt="Foto do Catioro" class="dog-photo">
         <div class="dog-name">Vira Lata</div>
         <div class="dog-temperament">Escandaloso, carente, disperso, medroso</div>
-    </div>
-`
+    </div>`
+
+export default { item }
